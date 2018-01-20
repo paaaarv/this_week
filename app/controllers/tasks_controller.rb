07f6640abaedc7e1bf80b693402d72f287bc89da
Parv Kumar <parvathikumar92@gmail.com>
2018-01-20 00:43:22 +0000

    new task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,16 @@
 class TasksController < ApplicationController
 
   get '/tasks' do
-    @tasks = Task.all 
+    @tasks = Task.all
+    erb :'/tasks/index'
   end
 
+  get '/tasks/new' do
+    erb :'/tasks/new'
+  end
 
+  post '/tasks/new' do
+  end
 
 
 
